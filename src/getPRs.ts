e8ee371e8d2d145fbc2b6720d4fd25923d4c0984
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-24 22:00:24 -0700

    add getPRs function

---
+++ src/getPRs.ts
@@ -0,0 +1,20 @@
+import Octokit from "@octokit/rest";
+import { writeFileSync } from "fs";
+
+import { pat } from "./pat";
+
+export const getPRs = () => {
+  const client = new Octokit({
+    auth: pat
+  });
+
+  const owner = "microsoft";
+  const repo = "accessibility-insights-web";
+
+  const options = client.pulls.list.endpoint.merge({ owner, repo, state: 'all' });
+  client.paginate(options).then(pulls => {
+    writeFileSync("msft-ai-web-pulls.json", JSON.stringify(pulls, null, 4), {
+      encoding: "utf8"
+    });
+  });
+};
