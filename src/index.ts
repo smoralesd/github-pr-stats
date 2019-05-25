4c8d5d7056b55d1b941e91e35724f786ba75de6c
Sebastian Morales <sebastian.moralesd@gmail.com>
2019-05-24 22:00:36 -0700

    clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-import Octokit from '@octokit/rest';
-import moment from 'moment';
+import { getPRs } from "./getPRs";
 
-import { pat } from './pat';
-
-const client = new Octokit({
-  auth: pat
-});
-
-const owner = 'microsoft';
-const repo = 'accessibility-insights-web';
-
-const days: number[] = [];
-
-client.pulls.list({
-    owner,
-    repo,
-    state: 'all',
-    per_page: 100,
-}).then(pulls => {
-    // console.log(pulls);
-    pulls.data.forEach(pull => {
-        let age = 'NA';
-        if (pull.state == 'closed') {
-            const mergedAt = moment(pull.merged_at);
-            const createdAt = moment(pull.created_at);
-            age = (mergedAt.diff(createdAt, 'days')).toString();
-            days.push(Number(age));
-        }
-
-        console.log('#', pull.number, 'user', pull.user.login, 'days', age);
-    });
-
-    const sum = days.reduce((accumulator, current) => accumulator + current,0);
-    console.log("average", sum / days.length);
-});
+getPRs();
